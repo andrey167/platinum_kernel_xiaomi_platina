7667fbfcfb8654574db6e443951d2c7e8351cb2e
joshuous <joshuous@gmail.com>
2018-07-09 17:27:52 +0200

    sched/boost: Update functions for newer Dynamic Schedtune Boost changes
    We now need to pass the functions a boost slot argument. Also we rename
    the functions to reflect that we intend to perform sched_boost.
    
    Change-Id: I84a63aea2c9035267095762804efabf7be6c66d5
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- kernel/sched/boost.c
+++ kernel/sched/boost.c
@@ -27,6 +27,10 @@
 
 unsigned int sysctl_sched_boost;
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+static int boost_slot;
+#endif // CONFIG_DYNAMIC_STUNE_BOOST
+
 #ifdef CONFIG_SCHED_HMP
 static enum sched_boost_policy boost_policy;
 static enum sched_boost_policy boost_policy_dt = SCHED_BOOST_NONE;
@@ -218,9 +222,9 @@ int sched_boost_handler(struct ctl_table *table, int write,
 #endif
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 		if (*data > 0)
-			stune_boost("top-app");
+			do_stune_sched_boost("top-app", &boost_slot);
 		else
-			reset_stune_boost("top-app");
+			reset_stune_boost("top-app", boost_slot);
 #endif
 	} else {
 		*data = old_val;
