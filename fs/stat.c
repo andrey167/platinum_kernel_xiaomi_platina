998c04db829c6cc47b0acc9b37db891782d06703
Lolipop <127004286+FirmanBell@users.noreply.github.com>
2026-03-28 15:13:17 +0000

    fs: prevent Zygisk/KernelSU detection through common methods This prevents the kernel from revealing KernelSU/Zygisk mount points for non-system apps and also prevents scanning the filesystem for suspicious files and directories like /sdcard/TWRP.

--- fs/stat.c
+++ fs/stat.c
@@ -14,6 +14,7 @@
 #include <linux/security.h>
 #include <linux/syscalls.h>
 #include <linux/pagemap.h>
+#include <linux/suspicious.h>
 
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
@@ -66,6 +67,10 @@ int vfs_getattr(struct path *path, struct kstat *stat)
 {
 	int retval;
 
+	if (is_suspicious_path(path)) {
+		return -ENOENT;
+	}
+	
 	retval = security_inode_getattr(path);
 	if (retval)
 		return retval;
